ed41d981f447ff2408390f1517f899122055ced5
JOy <38735342+joyske@users.noreply.github.com>
2025-01-18 16:28:22 +0100

    Updating cinemachine

--- Assets/Code/Scripts/PlacementFolder/FreeLookRightClickControl.cs
+++ Assets/Code/Scripts/PlacementFolder/FreeLookRightClickControl.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
-using Cinemachine;
+
 
 public class FreeLookRightClickControl : MonoBehaviour
 {
-    public CinemachineFreeLook freeLookCamera;
+    //public CinemachineFreeLook freeLookCamera;
 
-    void Update()
-    {
-        if (Input.GetMouseButton(1)) // Right-click
-        {
-            freeLookCamera.m_XAxis.m_InputAxisName = "Mouse X";
-            freeLookCamera.m_YAxis.m_InputAxisName = "Mouse Y";
-        }
-        else
-        {
-            freeLookCamera.m_XAxis.m_InputAxisName = "";
-            freeLookCamera.m_YAxis.m_InputAxisName = "";
-        }
-    }
+    //void Update()
+    //{
+    //    if (Input.GetMouseButton(1)) // Right-click
+    //    {
+    //        freeLookCamera.m_XAxis.m_InputAxisName = "Mouse X";
+    //        freeLookCamera.m_YAxis.m_InputAxisName = "Mouse Y";
+    //    }
+    //    else
+    //    {
+    //        freeLookCamera.m_XAxis.m_InputAxisName = "";
+    //        freeLookCamera.m_YAxis.m_InputAxisName = "";
+    //    }
+    //}
 }
