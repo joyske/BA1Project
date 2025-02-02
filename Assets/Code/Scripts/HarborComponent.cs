c64f4d3071b4eff09b5c20b31883da4a8c3aad7a
Krossa1 <felix.terlinden@gmail.com>
2025-02-02 23:07:45 +0100

    Fixes to multiple harbor components intersecting, re enabled harbor in level 3

--- Assets/Code/Scripts/HarborComponent.cs
+++ Assets/Code/Scripts/HarborComponent.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 public class HarborComponent : MonoBehaviour
 {
@@ -26,6 +27,10 @@ public class HarborComponent : MonoBehaviour
     private List<HarborComponent> harborComponents;
     private HarborComponent closestHarbor;
 
+    private float waveHeightAlpha;
+    private int harborsInRange;
+    private float lowestWaveHeightAlpha;
+
 
     void Start()
     {
@@ -45,21 +50,36 @@ public class HarborComponent : MonoBehaviour
     void Update()
     {
         currentDistance = Vector3.Distance(transform.position, playerRef.transform.position);
+        Debug.Log("Distance: " + currentDistance);
 
         if (isTargetHarbor)
         {
-            for (int i = 0; i < harborComponents.Count; i++) { if (harborComponents[i].currentDistance < closestHarbor.currentDistance) { closestHarbor = harborComponents[i]; } }
-
             if (playerRef == null) { playerRef = GameObject.FindGameObjectWithTag("Boat"); }
-            else
+            else { calculateCurrentWaveHeightAlpha(); }
+        }
+    }
+
+    void calculateCurrentWaveHeightAlpha()
+    {
+        currentWaveHeightAlpha = 1;
+        lowestWaveHeightAlpha = 1;
+
+        for (int i = 0; i < harborComponents.Count; i++)
+        {
+            if (harborComponents[i].currentDistance < harborComponents[i].fadeDistance)
             {
-                if (closestHarbor.currentDistance < closestHarbor.fadeDistance)
-                {
-                    currentWaveHeightAlpha = (closestHarbor.currentDistance / closestHarbor.fadeDistance);
-                    if (currentWaveHeightAlpha > closestHarbor.maxWaveHeightAlpha) { currentWaveHeightAlpha = closestHarbor.maxWaveHeightAlpha; }
-                    if (currentWaveHeightAlpha < closestHarbor.minWaveHeightAlpha) { currentWaveHeightAlpha = closestHarbor.minWaveHeightAlpha; }
-                }
-                else { currentWaveHeightAlpha = closestHarbor.maxWaveHeightAlpha; }
+                waveHeightAlpha = ((harborComponents[i].maxWaveHeightAlpha - harborComponents[i].minWaveHeightAlpha) * (harborComponents[i].currentDistance / harborComponents[i].fadeDistance) + harborComponents[i].minWaveHeightAlpha);
+                if (waveHeightAlpha > harborComponents[i].maxWaveHeightAlpha) { waveHeightAlpha = harborComponents[i].maxWaveHeightAlpha; }
+                if (waveHeightAlpha < harborComponents[i].minWaveHeightAlpha) { waveHeightAlpha = harborComponents[i].minWaveHeightAlpha; }
+                if (waveHeightAlpha < lowestWaveHeightAlpha) { lowestWaveHeightAlpha = waveHeightAlpha; }
+                
+            }
+
+            
+            if (i == harborComponents.Count - 1) 
+            { 
+                currentWaveHeightAlpha = lowestWaveHeightAlpha;
+                Debug.Log("Alpha: " + currentWaveHeightAlpha);
 
                 water.largeBand0Multiplier = defaultFirstBand * currentWaveHeightAlpha;
                 water.largeBand1Multiplier = defaultSecondBand * currentWaveHeightAlpha;
