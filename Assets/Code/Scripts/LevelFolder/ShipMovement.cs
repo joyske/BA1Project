9cbcf44c4e7f50af5de11e6201bc002ea46f6ca3
Krossa1 <felix.terlinden@gmail.com>
2025-02-04 15:25:22 +0100

    Boat cleanup pass

--- Assets/Code/Scripts/LevelFolder/ShipMovement.cs
+++ Assets/Code/Scripts/LevelFolder/ShipMovement.cs
@@ -6,62 +6,39 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
-    public Rigidbody rigidBodyRef;
+    //References
+    private Rigidbody rigidBodyRef;
+    private GameObject TurningWheel;
+    private PlayerInputManager PlayerInputManager;
 
+    //Adjustable movement parameters
     public float turningSpeed;
     public float maxForwardSpeed;
     public float maxBackwardSpeed;
     public float desiredSpeedChangeMultiplier;
-    
     public float turnDelay;
     public float CollidedMultiplier = 0.7f;
     public float WheelTurnSpeed;
 
+    //List of Floater points
     public Floater[] Floaters;
 
+    //Parameters used for movement calculation
+    private Vector3 desiredRotation;
     private Vector3 velocity = Vector3.zero;
-    private int CollidingElements = 0;
-    private float CurrentCollidedMultiplier = 1;
-
+    private int collidingElements = 0;
+    private float currentCollidedMultiplier = 1;
     [HideInInspector]
     public float currentDesiredSpeed;
-
-    private GameObject TurningWheel;
-    private PlayerInputManager PlayerInputManager;
-
-    private Vector3 DesiredRotation;
-    private Vector3 targetVelocity = Vector3.zero;
-    private Vector3 deltaVelocity;
-    
-
-
-    //Colliding elements reduce desired speed
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (!((collision.gameObject.tag == ("Ocean")) | (collision.gameObject.tag == ("Player"))))
-        { CollidingElements++; }
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (!((collision.gameObject.tag == ("Ocean")) | (collision.gameObject.tag == ("Player"))))
-        { currentDesiredSpeed = currentDesiredSpeed * (1 - (((1 - 0.2f) / CollidingElements)) * Time.deltaTime); }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (!((collision.gameObject.tag == ("Ocean")) | (collision.gameObject.tag == ("Player"))))
-        { CollidingElements--; }
-    }
 
 
     private void Start()
     {
         //Get references, hide cursor
+        rigidBodyRef = GetComponent<Rigidbody>();
         TurningWheel = GameObject.FindWithTag("TurningWheel");
         PlayerInputManager = GetComponent<PlayerInputManager>();
-
-        HideMouseCursor();
+        Cursor.visible = false;
     }
 
     private void FixedUpdate()
@@ -71,43 +48,38 @@ public class ShipMovement : MonoBehaviour
         {
             currentDesiredSpeed += (desiredSpeedChangeMultiplier * PlayerInputManager.movementInput.y) * Time.fixedDeltaTime;
 
+            //Clamp to min/max values
             if (currentDesiredSpeed > maxForwardSpeed) { currentDesiredSpeed = maxForwardSpeed; }
             if (currentDesiredSpeed < maxBackwardSpeed) { currentDesiredSpeed = maxBackwardSpeed; }
         }
 
         //Determine desired rotation force
         if (PlayerInputManager.movementInput.x != 0) 
-        { DesiredRotation = Vector3.SmoothDamp(DesiredRotation, new Vector3(0, (PlayerInputManager.movementInput.x) * turningSpeed, 0), ref velocity, turnDelay * Time.fixedDeltaTime); }
+        { desiredRotation = Vector3.SmoothDamp(desiredRotation, new Vector3(0, (PlayerInputManager.movementInput.x) * turningSpeed, 0), ref velocity, turnDelay * Time.fixedDeltaTime); }
         else 
         { 
-            DesiredRotation = Vector3.SmoothDamp(DesiredRotation, new Vector3(0, 0, 0), ref velocity, turnDelay * Time.fixedDeltaTime);
-            if(Vector3.Magnitude(DesiredRotation) <= 2)
-            { DesiredRotation = new Vector3(0, 0, 0); }
+            //If no sideway input, reduce desiredRotation and round towards 0 if below threshhold
+            desiredRotation = Vector3.SmoothDamp(desiredRotation, new Vector3(0, 0, 0), ref velocity, turnDelay * Time.fixedDeltaTime);
+            if(Vector3.Magnitude(desiredRotation) <= 2)
+            { desiredRotation = new Vector3(0, 0, 0); }
         }
 
 
         //Lower user input force if currently colliding
-        if (CollidingElements > 0) { CurrentCollidedMultiplier = CollidedMultiplier; }
-        else { CurrentCollidedMultiplier = 1; }
+        if (collidingElements > 0) { currentCollidedMultiplier = CollidedMultiplier; }
+        else { currentCollidedMultiplier = 1; }
 
         //Add torque based on user inputs
-        rigidBodyRef.AddTorque(DesiredRotation * CollidedMultiplier * Time.fixedDeltaTime, ForceMode.Acceleration);
-
-
-
-        targetVelocity = -transform.right * currentDesiredSpeed * CurrentCollidedMultiplier;
-        deltaVelocity = targetVelocity - rigidBodyRef.velocity;
+        rigidBodyRef.AddTorque(desiredRotation * CollidedMultiplier * Time.fixedDeltaTime, ForceMode.Acceleration);
 
         //Apply proportional force for each submerged floater based on user inputs
         foreach (var Floater in Floaters)
         {
-            if(Floater.isSubmerged || CollidingElements > 0)
+            if(Floater.isSubmerged || collidingElements > 0)
             {
-                rigidBodyRef.AddForce((-transform.right * currentDesiredSpeed * CurrentCollidedMultiplier * Time.fixedDeltaTime) / Floaters.Length, ForceMode.Acceleration);
+                rigidBodyRef.AddForce((-transform.right * currentDesiredSpeed * currentCollidedMultiplier * Time.fixedDeltaTime) / Floaters.Length, ForceMode.Acceleration);
             }
         }
-
-
 
         //Turn wheels proportional to current desired velocity
         TurningWheel.transform.Rotate(-Vector3.up * Time.deltaTime * (currentDesiredSpeed * WheelTurnSpeed), Space.Self);
@@ -115,13 +87,28 @@ public class ShipMovement : MonoBehaviour
         //Rehide cursor on enter window after alt + tab
         if (Input.GetMouseButtonDown(0) | Input.GetMouseButtonDown(1))
         {
-            HideMouseCursor();
+            Cursor.visible = false;
         }
     }
 
-    private void HideMouseCursor()
+
+    //Colliding elements reduce desired speed
+    private void OnCollisionEnter(Collision collision)
     {
-        Cursor.visible = false;
+        if (!((collision.gameObject.tag == ("Ocean")) | (collision.gameObject.tag == ("Player"))))
+        { collidingElements++; }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!((collision.gameObject.tag == ("Ocean")) | (collision.gameObject.tag == ("Player"))))
+        { currentDesiredSpeed = currentDesiredSpeed * (1 - (((1 - 0.2f) / collidingElements)) * Time.deltaTime); }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (!((collision.gameObject.tag == ("Ocean")) | (collision.gameObject.tag == ("Player"))))
+        { collidingElements--; }
     }
 }
 
